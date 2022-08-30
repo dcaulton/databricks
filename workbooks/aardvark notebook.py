61bdc7c73dece118c227b9040e41b4d6df480654
dcaulton@gmail.com <dcaulton@gmail.com>
2022-08-30 19:38:09 +0000

    learning how to do markdown in notebooks

--- workbooks/aardvark notebook.py
+++ workbooks/aardvark notebook.py
@@ -1,2 +1,27 @@
 # Databricks notebook source
+# DBTITLE 1,Feet First
+# FFeet first
 print("you must be wondering how this was named")
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # You need to stay cool
+
+# COMMAND ----------
+
+print('working on staying cool is easy')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## then you must do it
+# MAGIC - and
+# MAGIC - not 
+# MAGIC - hesitate
+
+# COMMAND ----------
+
+print('strike first, strike hard, no mercy')
